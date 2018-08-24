29cf2388c490d18920509e665588dec15b3b9a92
nmistic <nihilistimistic@gmail.com>
2018-08-24 16:05:27 +0530

    commenting is a bitch

--- creategestures.py
+++ creategestures.py
@@ -1,77 +1,149 @@
+# cv2 - real time computer vision library
 import cv2
 import numpy as np
 import os
 
+# dimensions of image
 image_x, image_y = 50, 50
 
+# video capture using default(0) camera - global
 cap = cv2.VideoCapture(0)
 
+# Gaussian Mixture-based Background/Foreground Segmentation Algorithm for motion analysis
+# The weights of the mixture represent the time proportions that those colours stay in the scene.
+# The probable background colours are the ones which stay longer and more static
+# Parameters -
+# history	Length of the history.
+# varThreshold	Threshold on the squared Mahalanobis distance between the pixel and the model to decide whether a
+# pixel is well described by the background model. This parameter does not affect the background update.
+# detectShadows	If true, the algorithm will detect shadows and mark them.
+# It decreases the speed a bit, so if you do not need this feature, set the parameter to false.
 fbag = cv2.createBackgroundSubtractorMOG2()
 
+# create folder if it does not exist
 def create_folder(folder_name):
     if not os.path.exists(folder_name):
         os.mkdir(folder_name)
 
+# Parameters -
+# g_id - gesture name
 def main(g_id):
 
+    # number of snaps to be captured
     total_pics = 1200
+    # locally declared VideoCapture
     cap = cv2.VideoCapture(0)
 
+    # x, y - starting indices of frame
+    # h, w - height and width of frame
     x, y, w, h = 300, 50, 350, 350
 
+    # create folder with g_id as label
     create_folder("gestures/" + str(g_id))
+    # counter for pictures
     pic_no = 0
+    # press c to set this bool to true and begin capturing
     flag_start_capturing = False
     frames = 0
 
     while True:
+        # next frame in camera via cap
+        # ret - return value from camera frame (true / false)
         ret, frame = cap.read()
+        # flip frame vertically - 1, horizontally - 0
         frame = cv2.flip(frame, 1)
+        # hsv - hue saturation value
+        # change pic from one color space to another
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
+        # create mask which captures colors with pixel intensities in the range of rgb(60,50,2) to rgb(255,150,25)
         mask2 = cv2.inRange(hsv, np.array([2, 50, 60]), np.array([25, 150, 255]))
+        # bit AND frame and mask
         res = cv2.bitwise_and(frame, frame, mask=mask2)
 
         gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+        # blur filter for image using Gaussian function
+        # specify the width and height of the kernel which should be positive and odd.
+        # We also should specify the standard deviation in the X and Y directions, sigmaX and sigmaY respectively.
+        # If only sigmaX is specified, sigmaY is taken as equal to sigmaX.
+        # If both are given as zeros, they are calculated from the kernel size.
+        # Gaussian filtering is highly effective in removing Gaussian noise from the image.
         median = cv2.GaussianBlur(gray, (5, 5), 0)
 
+        # structuring element or kernel which decides the nature of operation in dilation
         kernel_square = np.ones((5, 5), np.uint8)
 
+        # a pixel element is ‘1’ if atleast one pixel under the kernel is ‘1’.
+        # So it increases the white region in the image or size of foreground object increases.
         dilation = cv2.dilate(median, kernel_square, iterations=2)
 
+        # difference between dilation and erosion of an image
         opening = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel_square)
 
+        # First argument is the source image, which should be a grayscale image.
+        # Second argument is the threshold value which is used to classify the pixel values.
+        # Third argument is the maxVal which represents the value to be given if pixel value is more than
+        # (sometimes less than) the threshold value
         ret, thresh = cv2.threshold(opening, 30, 255, cv2.THRESH_BINARY)
         thresh = thresh[y:y + h, x:x + w]
 
+        # first one is source image, second is contour retrieval mode, third is contour approximation method.
+        # And it outputs the contours and hierarchy. contours is a Python list of all the contours in the image.
+        # Each individual contour is a Numpy array of (x,y) coordinates of boundary points of the object.
         contours = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[1]
 
         if len(contours) > 0:
+            # find max contour on basis on contourArea
             contour = max(contours, key=cv2.contourArea)
+            # contourArea must be grateer than 10000 and frames are more than 50
             if cv2.contourArea(contour) > 10000 and frames > 50:
+                # find the most prominent contour in the image and crop it to the bounding rectangle
                 x1, y1, w1, h1 = cv2.boundingRect(contour)
+                # increment pic_no
                 pic_no += 1
+                # crop the frame
                 save_img = thresh[y1:y1 + h1, x1:x1 + w1]
+                # enter border if h and w of frame do not match
                 if w1 > h1:
+                    # copyMakeBorder( src, dst, top, bottom, left, right, borderType, value)
                     save_img = cv2.copyMakeBorder(save_img, int((w1 - h1) / 2), int((w1 - h1) / 2), 0, 0,
                                                   cv2.BORDER_CONSTANT, (0, 0, 0))
                 elif h1 > w1:
                     save_img = cv2.copyMakeBorder(save_img, 0, 0, int((h1 - w1) / 2), int((h1 - w1) / 2),
                                                   cv2.BORDER_CONSTANT, (0, 0, 0))
+                # fill all pics to 50 x 50 pixel size
                 save_img = cv2.resize(save_img, (image_x, image_y))
 
+                # display text on the frame
+                # image
+                # Text data
+                # Position coordinates - bottom-left corner
+                # Font type
+                # Font Scale
+                # regular things like color, thickness, lineType etc
                 cv2.putText(frame, "Capturing...", (30, 60), cv2.FONT_HERSHEY_TRIPLEX, 2, (127, 255, 255))
 
+                # writing the captured image to gestures/g_id/pic_no.jpg
                 cv2.imwrite("gestures/" + str(g_id) + "/" + str(pic_no) + ".jpg", save_img)
 
+        # Parameters -
+        # image
+        # top left
+        # bottom right
+        # color code
+        # line type
+        # mark the bounding rectangle of capture frame
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
         cv2.putText(frame, str(pic_no), (30, 400), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (127, 127, 255))
         cv2.imshow("Capturing gesture", frame)
         cv2.imshow("thresh", thresh)
 
+        # wait for user keyboard input
+        # delay - 1 ms delay
         keypress = cv2.waitKey(1)
 
+        # if key press is 'c', start capture
         if keypress == ord('c'):
             if flag_start_capturing == False:
                 flag_start_capturing = True
--- training.py
+++ training.py
@@ -1,32 +1,83 @@
 import numpy as np
 
+# neural networks library for use on top of tensorflow
 from keras import layers
+
+# Input() is used to instantiate a Keras tensor.
+# A Keras tensor is a tensor object from the underlying backend (Theano or TensorFlow),
+# which we augment with certain attributes that allow us
+# to build a Keras model just by knowing the inputs and outputs of the model
+
+# Dense - regular densely-connected NN layer
+
+# Activation - Activation function to be used as a layer or activation argument in other layers
+
+# ZeroPadding2D - This layer can add rows and columns of zeros at the top, bottom, left and right side
+# of an image tensor
+
+# BatchNormalization - Normalize the activations of the previous layer at each batch,
+# i.e. applies a transformation that maintains the mean activation close to 0 and
+# the activation standard deviation close to 1
+
+# Flatten - flatten the input
+
+# Conv2D - 2D convolution layer (e.g. spatial convolution over images)
 from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
 
+# AveragePooling2D -  Its function is to progressively reduce the spatial size of the representation to
+# reduce the amount of parameters and computation in the network, and hence to also control overfitting
+
+# MaxPooling2D - Max pooling is done by applying a max filter to (usually)
+# non-overlapping subregions of the initial representation.
+
+# Dropout - ignoring units (i.e. neurons) during the training phase of certain set of neurons which is chosen at random
+# GlobalMaxPooling2D - MaxPooling2D with pool size = input size
+# GlobalAveragePooling2D - AveragePooling2D with pool size = input size
 from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
 
+# keras utility functions
 from keras.utils import np_utils
+# Sequential - linear stack of layers
 from keras.models import Sequential
+# callback is a set of functions to be applied at given stages of the training procedure
+# ModelCheckpoint save model after every epoch
 from keras.callbacks import ModelCheckpoint
 import pandas as pd
 
+# pandas - data manipulation and analysis
 import keras.backend as K
 
 
+# image_x, image_y - dimensions of image
 def keras_model(image_x, image_y):
+    # number of classes among which to classify
     num_of_classes = 12
+    # set up a sequential model and add cnn layers
     model = Sequential()
+    # Parameters -
+    # filters - number of outputs from conv2d layer
+    # kernel size
+    # image shape
+    # activation function - relu
     model.add(Conv2D(32, (5, 5), input_shape=(image_x, image_y, 1), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'))
     model.add(Conv2D(64, (5, 5), activation='sigmoid'))
     model.add(MaxPooling2D(pool_size=(5, 5), strides=(5, 5), padding='same'))
     model.add(Flatten())
+    # units - number of neurons in the layer
     model.add(Dense(1024, activation='relu'))
+    # 60% dropout rate
     model.add(Dropout(0.6))
+    # final softmax layer to classify image
     model.add(Dense(num_of_classes, activation='softmax'))
 
+    # configure the model for training
+    # optimizer - adam
+    # loss - categorical crossentropy
+    # metrics - accuracy
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    filepath = "gesturerecog.h5"
+    filepath = "emojinator.h5"
+    # save model in filepath
     checkpoint1 = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
     callbacks_list = [checkpoint1]
 
@@ -34,6 +85,7 @@ def keras_model(image_x, image_y):
 
 
 def main():
+    # read csv for creating train and test data
     data = pd.read_csv("train_foo.csv")
     dataset = np.array(data)
     np.random.shuffle(dataset)
@@ -48,6 +100,7 @@ def main():
     X_test = X[12000:13201, :]
     X_test = X_test / 255.
 
+    # Reshape
     Y = Y.reshape(Y.shape[0], 1)
     Y_train = Y[0:12000, :]
     Y_train = Y_train.T
@@ -63,6 +116,7 @@ def main():
     image_x = 50
     image_y = 50
 
+    # Converts a class vector (integers) to binary class matrix
     train_y = np_utils.to_categorical(Y_train)
     test_y = np_utils.to_categorical(Y_test)
     train_y = train_y.reshape(train_y.shape[1], train_y.shape[2])
@@ -78,7 +132,7 @@ def main():
     scores = model.evaluate(X_test, test_y, verbose=0)
     print("CNN Error: %.2f%%" % (100 - scores[1] * 100))
 
-    model.save('gesturerecog.h5')
+    model.save('emojinator.h5')
 
 
 main()
